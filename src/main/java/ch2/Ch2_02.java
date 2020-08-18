09cff2d02d2128f19a8854666b61ca57bbee1c3a
Alexey Baryzhikov <aleksiarts@gmail.com>
2020-08-18 23:39:02 +0300

    Refactor Ch2_02

--- src/main/java/ch2/Ch2_02.java
+++ src/main/java/ch2/Ch2_02.java
@@ -1,28 +1,34 @@
 package ch2;
 
 import io.reactivex.rxjava3.core.Observable;
-import io.reactivex.rxjava3.core.ObservableOnSubscribe;
+import io.reactivex.rxjava3.disposables.Disposable;
+
+import static util.Utils.log;
 
 public class Ch2_02 {
 
     public static void main(String[] args) {
-        Observable.create((ObservableOnSubscribe<String>) emitter -> {
+        Observable<String> publisher = Observable.create(emitter -> {
             try {
                 emitter.onNext("Alpha");
                 emitter.onNext("Beta");
                 emitter.onNext("Gamma");
+                log("SOURCE", "Complete");
                 emitter.onComplete();
             } catch (Throwable th) {
+                log("SOURCE", "Failed with " + th);
                 emitter.onError(th);
             }
-        })
-                .subscribe(
-                        s -> {
-                            if (s.length() < 5) throw new IllegalArgumentException();
-                            System.out.println(s);
-                        },
-                        Throwable::printStackTrace
-                )
-                .dispose();
+        });
+
+        Disposable d = publisher.subscribe(
+                it -> {
+                    if (it.length() < 5) throw new IllegalArgumentException();
+                    log("OBSERVER", "onNext " + it);
+                },
+                th -> log("OBSERVER", "onError " + th)
+        );
+
+        d.dispose();
     }
 }
