4a639552bed4abb24d4dd28c98ee48dc3c76d2e0
Alexey Baryzhikov <aleksiarts@gmail.com>
2020-10-18 17:02:26 +0300

    Add TruncateLines

---
+++ src/main/java/sandbox/TruncateLines.java
@@ -0,0 +1,14 @@
+package sandbox;
+
+import io.reactivex.rxjava3.core.Observable;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class TruncateLines {
+    public static void main(String[] args) {
+        Observable.fromStream(new BufferedReader(new InputStreamReader(System.in)).lines())
+                .map(s -> s.length() < 10 ? s : s.substring(0, 10))
+                .blockingSubscribe(System.out::println, Throwable::printStackTrace);
+    }
+}
