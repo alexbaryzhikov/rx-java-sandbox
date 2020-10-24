fefdf5960abfcfee8801d8ff9e61d9dd9437150b
Alexey Baryzhikov <aleksiarts@gmail.com>
2020-10-24 19:38:38 +0300

    Add another fibs implementation

--- src/main/java/sandbox/Fibs.java
+++ src/main/java/sandbox/Fibs.java
@@ -4,16 +4,23 @@ import io.reactivex.rxjava3.core.Observable;
 
 public class Fibs {
 
-    static Observable<Integer> fibs() {
+    private static Observable<Integer> fibs() {
         return Observable.just(0, 1)
                 .concatWith(Observable.defer(() -> fibs()
                         .window(2, 1)
                         .flatMapSingle(it -> it.reduce(0, Integer::sum))));
     }
 
+    private static Observable<Integer> fibs2() {
+        return Observable.just(0)
+                .repeat()
+                .scan(new int[]{0, 1}, (a, i) -> new int[]{a[1], a[0] + a[1]})
+                .map(a -> a[0]);
+    }
+
     public static void main(String[] args) {
-        fibs().take(10)
-                .subscribe(System.out::println)
-                .dispose();
+        fibs().take(10).blockingSubscribe(it -> System.out.print(it + " "));
+        System.out.println();
+        fibs2().take(10).blockingSubscribe(it -> System.out.print(it + " "));
     }
 }
