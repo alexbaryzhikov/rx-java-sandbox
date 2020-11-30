213fa3757e61ebb9c499f6222d56aa44f43225ae
Alexey Baryzhikov <aleksiarts@gmail.com>
2020-11-30 23:58:15 +0300

    Finish SquashDuplicates

--- src/main/java/sandbox/SquashDuplicates.java
+++ src/main/java/sandbox/SquashDuplicates.java
@@ -1,40 +1,54 @@
 package sandbox;
 
-import io.reactivex.Observable;
-import io.reactivex.schedulers.Schedulers;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 import java.util.Queue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 
 import static util.Utils.*;
 
+@SuppressWarnings("UnusedReturnValue")
 public class SquashDuplicates {
 
     public static void main(String[] args) {
-        Queue<Task> queue = new LinkedBlockingQueue<>();
+        LinkedBlockingQueue<Task> queue = new LinkedBlockingQueue<>();
         startConsumer(queue);
         startProducer(queue);
         sleep(5000);
     }
 
-    private static void startProducer(Queue<Task> queue) {
-        Observable.interval(50, TimeUnit.MILLISECONDS)
-                .take(20)
-                .subscribe(it -> {
-                    log("PRODUCER", "Create " + it);
-                    queue.add(new Task("Foo", it.intValue(), randomLong(100, 200)));
-                });
-    }
-
-    private static void startConsumer(Queue<Task> queue) {
-        Observable.fromIterable(queue)
+    private static Disposable startConsumer(LinkedBlockingQueue<Task> queue) {
+        return Observable.<Task>generate(emitter -> emitter.onNext(queue.take()))
                 .subscribeOn(Schedulers.computation())
                 .subscribe(it -> {
-                    log("CONSUMER", "Start " + it);
+                    log("CONSUMER", "Start  " + it);
                     sleep(it.duration);
                     log("CONSUMER", "Finish " + it);
                 });
+    }
+
+    private static Disposable startProducer(Queue<Task> queue) {
+        List<String> keys = Arrays.asList("foo", "bar", "baz");
+        return Observable.interval(50, TimeUnit.MILLISECONDS)
+                .take(20)
+                .subscribe(it -> {
+                    String key = keys.get(randomInt(0, keys.size()));
+                    Task task = new Task(key, it.intValue(), randomLong(100, 200));
+                    log("PRODUCER", "Create " + task);
+                    squash(queue, task);
+                });
+    }
+
+    private static void squash(Queue<Task> queue, Task task) {
+        queue.remove(task);
+        queue.add(task);
+        log("SQUASH", queue.toString());
     }
 
     private static class Task {
@@ -49,12 +63,21 @@ public class SquashDuplicates {
         }
 
         @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Task task = (Task) o;
+            return key.equals(task.key);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(key);
+        }
+
+        @Override
         public String toString() {
-            return "Task{" +
-                    "key='" + key + '\'' +
-                    ", id=" + id +
-                    ", duration=" + duration +
-                    '}';
+            return key + ':' + id + ':' + duration;
         }
     }
 }
